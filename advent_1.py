15f52cbf337892d84ced6ed6b97cdc73e4617739
Veggis1077 <vegard.reirag@gmail.com>
2022-12-02 10:21:44 +0100

    Lagt til den ideelle losningen på advent_1

---
+++ advent_1.py
@@ -0,0 +1,18 @@
+liste = []
+with open("textfil-kopi.txt") as f:
+    for i in f: liste.append(i)
+for i in range(len(liste)): liste[i] = (liste[i][:-1])
+maxsum = 0
+a = 0
+nyliste = []
+for i in liste:
+    if i == "":
+        nyliste.append(a)
+        if maxsum < a:
+            maxsum = a
+            a = 0
+        else: a = 0
+    else: a+=int(i)
+nyliste.sort()
+print(maxsum)
+print(maxsum + nyliste[-2] + nyliste[-3])
